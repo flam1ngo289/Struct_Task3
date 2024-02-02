f99a3522ccac581344065866a86bf8baef76ed32
flam1ngo <kris.s.03@mail.ru>
2024-02-02 18:37:53 +0300

    Initial commit

--- Task3/main.cpp
+++ Task3/main.cpp
@@ -1,14 +1,39 @@
-//
-//  main.cpp
-//  Task3
-//
-//  Created by Кристина Скворцова on 02.02.2024.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+struct adress {
+    std::string city;
+    std::string street;
+    int numHouse;
+    int numFlat;
+    int index;
+};
+
+void print(adress &adr){
+    std::cout << "Город: " << adr.city << std::endl;
+    std::cout << "Улица: " << adr.street << std::endl;
+    std::cout << "Номер дома: " << adr.numHouse << std::endl;
+    std::cout << "Номер квартиры: " << adr.numFlat << std::endl;
+    std::cout << "Индекс: " << adr.index << std::endl;
+}
+
+int main(){
+    adress Moscow;
+    Moscow.city = "Москва";
+    Moscow.street = "Арбат";
+    Moscow.numHouse = 12;
+    Moscow.numFlat = 8;
+    Moscow.index = 123456;
+
+    adress Izhewsk;
+    Izhewsk.city = "Ижевск";
+    Izhewsk.street = "Пушкина";
+    Izhewsk.numHouse = 59;
+    Izhewsk.numFlat = 143;
+    Izhewsk.index = 953769;
+
+    print(Moscow);
+    std::cout << std::endl;
+    print(Izhewsk);
+
+
 }
