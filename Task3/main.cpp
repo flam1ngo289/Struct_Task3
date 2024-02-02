0a521d13aae60c02ae054fbd3e2bd28ee3753314
flam1ngo <kris.s.03@mail.ru>
2024-02-02 18:37:14 +0300

    Initial Commit

---
+++ Task3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Task3
+//
+//  Created by Кристина Скворцова on 02.02.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
